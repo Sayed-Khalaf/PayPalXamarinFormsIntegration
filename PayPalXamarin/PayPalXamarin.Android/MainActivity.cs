ffb74c1a0864c789266c1a1e9a8fe747ed691147
Sayed Khalaf <Sayed.Khalaf@emeint.net>
2018-12-25 17:08:18 +0200

    Commit Changes

---
+++ PayPalXamarin/PayPalXamarin.Android/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using PayPal.Forms.Abstractions;
+using PayPal.Forms;
+using Android.Content;
+
+namespace PayPalXamarin.Droid
+{
+    [Activity(Label = "PayPalXamarin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            //.. PayPal Configuration Setup ..//
+            var config = new PayPalConfiguration(PayPalEnvironment.Sandbox, "YOUR CLIENT ID HERE")
+            {
+
+                AcceptCreditCards = true,
+                MerchantName = "Your Merchant Name Here",
+                //Your privacy policy Url
+                MerchantPrivacyPolicyUri = "https://www.example.com/privacy",
+                //Your user agreement Url
+                MerchantUserAgreementUri = "https://www.example.com/legal"
+            };
+            CrossPayPalManager.Init(config, this);
+
+            LoadApplication(new App());
+        }
+
+        //.. Very Importatnt To Override OnActivityResult & OnDestroy ..//
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            PayPalManagerImplementation.Manager.OnActivityResult(requestCode, resultCode, data);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            PayPalManagerImplementation.Manager.Destroy();
+        }
+
+    }
+}
+
---
+++ PayPalXamarin/PayPalXamarin/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using PayPal.Forms;
+using PayPal.Forms.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PayPalXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            //TestPayPal().Wait();
+		}
+
+        async void OnTestClick(object sender, EventArgs args)
+        {
+            await TestPayPal();
+        }
+
+        public async Task TestPayPal()
+        {
+            try
+            {
+                var result = await CrossPayPalManager.Current.Buy(new PayPalItem("Test Product", new Decimal(9), "USD"), new Decimal(0));
+
+                if (result.Status == PayPalStatus.Cancelled)
+                {
+                    Debug.WriteLine("Cancelled");
+                }
+                else if (result.Status == PayPalStatus.Error)
+                {
+                    Debug.WriteLine(result.ErrorMessage);
+                }
+                else if (result.Status == PayPalStatus.Successful)
+                {
+                    Debug.WriteLine(result.ServerResponse.Response.Id);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+	}
+}
